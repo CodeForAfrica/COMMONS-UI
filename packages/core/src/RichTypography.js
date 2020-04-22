7b080f0604b14382a9eb71cc18bafdb0a75b21be
Clemence Kyara <clemence@codeforafrica.org>
2020-04-22 16:22:08 +0300

    Cleanup and make RichTypography reusable

--- packages/core/src/RichTypography.js
+++ packages/core/src/RichTypography.js
@@ -13,34 +13,38 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function RichTypography({ children, variant, ...props }) {
+const RichTypography = React.forwardRef(function RichTypography(
+  { children, component, ...props },
+  ref
+) {
   const classes = useStyles(props);
 
   if (!children) {
     return null;
   }
-
   return (
     <Typography
-      variant={variant}
-      component="div"
-      className={classes.root}
+      component={component}
       dangerouslySetInnerHTML={{
         __html: children,
       }}
       {...props}
+      ref={ref}
+      className={classes.root}
     />
   );
-}
+});
 
 RichTypography.propTypes = {
   children: PropTypes.string,
-  variant: PropTypes.string,
+  component: PropTypes.elementType,
 };
 
 RichTypography.defaultProps = {
   children: null,
-  variant: "body1",
+  // We default to `div` to allow other block elements like <p> to be used inside
+  // `children`
+  component: "div",
 };
 
 export default RichTypography;
