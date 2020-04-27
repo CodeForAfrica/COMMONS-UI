69755e2f3d340c3e6dd91d7fd7faa59b8c9ab3e9
Clemence Kyara <clemence@codeforafrica.org>
2020-04-27 16:17:28 +0300

    Use ListItem in StoryList

--- packages/core/src/StoryList/index.js
+++ packages/core/src/StoryList/index.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 import PropTypes from "prop-types";
 
-import { Grid, Typography, makeStyles } from "@material-ui/core";
+import clsx from "clsx";
 
-import List from "./StoryList";
+import { GridListTile, makeStyles } from "@material-ui/core";
+
+import GridList from "../ScrollableGridList";
+import Story from "../ListItem";
 
 const useStyles = makeStyles(() => ({
   root: {
-    flexGrow: 1,
-  },
-  heading: {
-    width: "100%",
-    marginBottom: "2rem",
+    zIndex: 1,
+    width: "calc(((100vw - 100%) / 2) + 100%)",
   },
   title: {},
   description: {},
+  story: {},
+  storyDescription: {},
+  storyLink: {},
+  storyName: {},
+  storyPicture: {},
+  storyTitle: {},
   stories: {},
   storiesGridList: {},
   storiesScrollBar: {},
@@ -22,79 +28,93 @@ const useStyles = makeStyles(() => ({
 
 function StoryList({
   cellHeight,
-  description,
   height,
   lg,
   md,
-  minHeight,
+  onClick,
   sm,
-  spacing,
+  storyClassCount,
+  storyClassPrefix,
   stories,
-  title,
   xs,
   ...props
 }) {
   const classes = useStyles(props);
+  const rootRef = useRef(null);
 
   if (!stories) {
     return null;
   }
   return (
-    <Grid container className={classes.root}>
-      <Grid
-        item
-        xs={12}
-        container
-        direction="row"
-        className={classes.heading}
-        justify="flex-start"
-        alignItems="center"
+    <div className={classes.root} ref={rootRef}>
+      <GridList
+        classes={{
+          root: classes.profiles,
+          gridList: classes.profilesGridList,
+          scrollBar: classes.profilesScrollBar,
+        }}
+        cellHeight={cellHeight}
+        height={height}
+        lg={lg}
+        md={md}
+        sm={sm}
+        xs={xs}
       >
-        <Grid item>
-          <Typography variant="h2" className={classes.title}>
-            {title}
-          </Typography>
-        </Grid>
-
-        <Grid item>
-          <Typography variant="body1" className={classes.description}>
-            {description}
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid item xs={12}>
-        <List
-          classes={{
-            root: classes.stories,
-            gridList: classes.storiesGridList,
-            scrollBar: classes.storiesScrollBar,
-          }}
-          cellHeight={cellHeight}
-          height={height}
-          lg={lg}
-          md={md}
-          minHeight={minHeight}
-          sm={sm}
-          spacing={spacing}
-          stories={stories}
-          xs={xs}
-        />
-      </Grid>
-    </Grid>
+        {stories.map((story, index) => (
+          <GridListTile key={story.id}>
+            <Story
+              key={story.title}
+              classes={{
+                root: clsx(classes.story, {
+                  [`${storyClassPrefix}${
+                    index % storyClassCount
+                  }`]: storyClassCount,
+                }),
+                description: classes.storyDescription,
+                link: classes.storyLink,
+                name: classes.storyName,
+                picture: classes.storyPicture,
+                title: classes.storyTitle,
+              }}
+              height={cellHeight}
+              onClick={() => onClick && onClick(index)}
+              description={story.description}
+              image={story.image}
+              link={story.link}
+              name={story.name}
+              title={story.title}
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
   );
 }
 
 StoryList.propTypes = {
   cellHeight: PropTypes.number,
-  description: PropTypes.string.isRequired,
   height: PropTypes.number,
   lg: PropTypes.number,
   md: PropTypes.number,
-  minHeight: PropTypes.number,
+  onClick: PropTypes.func,
+  storyClassCount: PropTypes.number,
+  storyClassPrefix: PropTypes.string,
+  stories: PropTypes.arrayOf(
+    PropTypes.shape({
+      description: PropTypes.string,
+      image: PropTypes.shape({
+        description: PropTypes.string,
+        url: PropTypes.string.isRequired,
+      }),
+      link: PropTypes.shape({
+        title: PropTypes.string,
+        url: PropTypes.string.isRequired,
+      }),
+      name: PropTypes.string,
+      title: PropTypes.string,
+    })
+  ).isRequired,
   sm: PropTypes.number,
-  spacing: PropTypes.number,
-  stories: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
-  title: PropTypes.string.isRequired,
   xs: PropTypes.number,
 };
 
@@ -103,9 +123,10 @@ StoryList.defaultProps = {
   height: 370, // 23.125rem
   lg: undefined,
   md: 3.3,
-  minHeight: undefined,
+  onClick: undefined,
   sm: undefined,
-  spacing: undefined,
+  storyClassCount: 3,
+  storyClassPrefix: "story-",
   xs: 1.3,
 };
 
