b297ba34d58004c63940e919d1908fa4d5f768aa
Clemence Kyara <clemence@codeforafrica.org>
2020-04-28 14:30:06 +0300

    Fix icon size + expose links & text classes

--- packages/core/src/Footer/StayInTouch.js
+++ packages/core/src/Footer/StayInTouch.js
@@ -14,26 +14,29 @@ const useStyles = makeStyles((theme) => ({
       textAlign: "left",
     },
   },
-  text: {
-    fontWeight: "bold",
-    lineHeight: 4.3125,
+  icon: {
+    width: "1.375rem",
+    height: "1.375rem",
+    objectFit: "contain",
+  },
+  link: {
+    display: "inline-block",
+    padding: "0 0.625rem",
+  },
+  links: {
     "& > a": {
       display: "inline-block",
-      padding: "0 1rem",
       borderRight: "1px solid white",
     },
     "& > a:last-of-type": {
       border: "none",
     },
+    [theme.breakpoints.up("md")]: {
+      justifyContent: "flex-start",
+      width: "auto",
+    },
   },
-  icon: {
-    width: "2rem",
-    height: "2rem",
-    objectFit: "contain",
-    verticalAlign: "middle",
-  },
-  iconContainer: {},
-  links: {},
+  text: {},
 }));
 
 function StayInTouch({ support, socialMedia, ...props }) {
@@ -52,39 +55,44 @@ function StayInTouch({ support, socialMedia, ...props }) {
           color="textSecondary"
           className={classes.text}
         >
-          Stay in touch with us &nbsp;
+          Stay in touch with us @ &nbsp;
         </Typography>
       </Grid>
-      <Grid item xs={12} md="auto" className={classes.content}>
-        <Typography variant="caption" className={classes.text}>
-          {support && (
-            <A
-              href={`mailto:${support.email}`}
-              color="textSecondary"
-              className={classes.links}
-            >
-              <img
-                src={support.image.url}
-                alt={support.image.alt}
-                className={classes.icon}
-              />
-            </A>
-          )}
-          {socialMedia.map((media) => (
-            <A
-              key={media.url}
-              href={media.url}
-              color="textSecondary"
-              className={classes.links}
-            >
-              <img
-                src={media.image.url}
-                alt={media.image.alt}
-                className={classes.icon}
-              />
-            </A>
-          ))}
-        </Typography>
+      <Grid
+        item
+        xs={12}
+        md="auto"
+        container
+        justify="center"
+        className={classes.links}
+      >
+        {support && (
+          <A
+            href={`mailto:${support.email}`}
+            color="textSecondary"
+            className={classes.link}
+          >
+            <img
+              src={support.image.url}
+              alt={support.image.alt}
+              className={classes.icon}
+            />
+          </A>
+        )}
+        {socialMedia.map((media) => (
+          <A
+            key={media.url}
+            href={media.url}
+            color="textSecondary"
+            className={classes.link}
+          >
+            <img
+              src={media.image.url}
+              alt={media.image.alt}
+              className={classes.icon}
+            />
+          </A>
+        ))}
       </Grid>
     </Grid>
   );
