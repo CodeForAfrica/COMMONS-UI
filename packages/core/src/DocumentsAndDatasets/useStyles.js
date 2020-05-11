ff59c5860f6cbf413dddf590ae5ce2bbd5a948cc
Clemence Kyara <clemence@codeforafrica.org>
2020-05-11 11:46:39 +0300

    Update classes

--- packages/core/src/DocumentsAndDatasets/useStyles.js
+++ packages/core/src/DocumentsAndDatasets/useStyles.js
@@ -2,50 +2,41 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(({ breakpoints, palette }) => ({
   root: {
-    flexGrow: 1,
     backgroundColor: palette.primary.main,
+    [breakpoints.up("md")]: {
+      backgroundColor: palette.primary.main,
+      background: `linear-gradient(to right, ${palette.primary.main} 0%, ${palette.primary.main} 50%, rgba(255, 255, 255, 0.1) 50%, rgba(255, 255, 255, 0.1) 100%);`,
+    },
   },
-  section: {
-    position: "relative",
-  },
-  background: {},
-  backgroundDatasets: {
-    background: "rgba(255, 255, 255, 0.1)",
-  },
-  backgroundDocuments: {},
+  section: {},
+  container: {},
   content: {
     [breakpoints.up("md")]: {
-      position: "absolute",
-      top: 0,
+      background: `linear-gradient(to right, ${palette.primary.main} 0%, ${palette.primary.main} 50%, transparent 50%, transparent 100%);`,
     },
   },
-  contentDatasets: {
+  contents: {},
+  contentsDatasets: {
+    backgroundColor: "rgba(255, 255, 255, 0.1)",
     [breakpoints.up("md")]: {
-      background: "inherit",
+      background: "unset",
     },
   },
-  contentDocuments: {
-    [breakpoints.up("md")]: {
-      background: "inherit",
-    },
-  },
-  contentHeading: {
-    marginTop: "5rem",
-  },
+  contentsDocuments: {},
   datasets: {},
   datasetsContentTitle: {},
   datasetsDescription: {},
   datasetsIcon: {},
   datasetsLink: {},
-  description: {
-    marginTop: "1rem",
-  },
+  description: {},
   documents: {},
   documentsContentTitle: {},
   documentsDescription: {},
   documentsIcon: {},
   documentsLink: {},
+  heading: {},
   highlight: {},
+  subtitle: {},
   title: {},
 }));
 
