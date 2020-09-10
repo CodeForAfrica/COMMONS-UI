5d5847db28b5d8631498590d23f868d5bd5bd4a2
Obed Amoasi <obedamoasi@gmail.com>
2020-09-10 15:05:37 +0000

    make initiative optional

--- packages/core/src/Footer/About/index.js
+++ packages/core/src/Footer/About/index.js
@@ -6,22 +6,27 @@ import useStyles from "../useStyles";
 import About from "./About";
 import Initiative from "./Initiative";
 
-function Index({ about, initiative, ...props }) {
+function Index({ children, initiative, ...props }) {
   const classes = useStyles(props);
   return (
     <div className={classes.root}>
-      <About classes={{ about: classes.about }}>{about}</About>
+      <About classes={{ about: classes.about }}>{children}</About>
       <br />
-      <Initiative classes={{ initiative: classes.initiative }}>
-        {initiative}
-      </Initiative>
+      {initiative && (
+        <Initiative classes={{ initiative: classes.initiative }}>
+          {initiative}
+        </Initiative>
+      )}
     </div>
   );
 }
 
 Index.propTypes = {
-  about: PropTypes.string.isRequired,
-  initiative: PropTypes.node.isRequired,
+  children: PropTypes.string.isRequired,
+  initiative: PropTypes.node,
+};
+Index.defaultProps = {
+  initiative: null,
 };
 
 export default Index;
