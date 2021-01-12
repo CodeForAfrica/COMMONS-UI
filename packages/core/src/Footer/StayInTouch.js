ab5538ac637f1fd9a4734d06e654f02fcedd2ff9
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-01-12 09:02:35 +0300

    temp to do notes

--- packages/core/src/Footer/StayInTouch.js
+++ packages/core/src/Footer/StayInTouch.js
@@ -43,6 +43,10 @@ const useStyles = makeStyles((theme) => ({
 function StayInTouch({ options, support, socialMedia, title, ...props }) {
   const classes = useStyles(props);
 
+  // TO DO
+  // should render with only wanted props and not all
+  // styling flexibility + control
+
   return (
     <Grid
       container
