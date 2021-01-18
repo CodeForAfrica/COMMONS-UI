18a597de09ee94d917cfafc240a39bc5b67308c7
nerdlet <brndnyokabi@gmail.com>
2021-01-18 17:21:10 +0300

    Add scrollable gridlist

--- stories/components.stories.js
+++ stories/components.stories.js
@@ -23,6 +23,7 @@ import {
   QuickLinks,
   ScrollBar,
   NavigationButton,
+  ScrollableGridList,
 } from "@commons-ui/core";
 
 import { makeStyles } from "@material-ui/core/styles";
@@ -62,19 +63,19 @@ const tileData = [
     img: personImage,
   },
   {
-    title: "Another Example title two",
+    title: "Example title two",
     img: personImage,
   },
   {
-    title: "Another Example title three",
+    title: "Example title three",
     img: personImage,
   },
   {
-    title: "Another Example title four",
+    title: "Example title four",
     img: personImage,
   },
   {
-    title: "Another Example title four",
+    title: "Example title five",
     img: personImage,
   },
   {
@@ -145,6 +146,48 @@ storiesOf("Components/Navigation", module)
           ))}
         </GridList>
       </ScrollBar>
+    );
+  })
+  .add("ScrollableGridList", () => {
+    const classes = makeStyles((theme) => ({
+      root: {},
+      scrollableGridList: {
+        flexWrap: "nowrap",
+        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+        transform: "translateZ(0)",
+      },
+      title: {
+        color: theme.palette.primary.light,
+      },
+      titleBar: {
+        background:
+          "linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
+      },
+      scrollBar: {},
+    }))();
+    return (
+      <ScrollableGridList
+        classes={{
+          root: classes.root,
+          gridList: classes.scrollableGridList,
+          scrollBar: classes.scrollBar,
+        }}
+      >
+        <GridList className={classes.gridList} cols={4}>
+          {tileData.map((tile) => (
+            <GridListTile key={tile.img}>
+              <img src={tile.img} alt={tile.title} />
+              <GridListTileBar
+                title={tile.title}
+                classes={{
+                  root: classes.titleBar,
+                  title: classes.title,
+                }}
+              />
+            </GridListTile>
+          ))}
+        </GridList>
+      </ScrollableGridList>
     );
   });
 
