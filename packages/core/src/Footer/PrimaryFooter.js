72984c923178c0af3c8b2ab25c10901d973d0124
Obed Amoasi <obedamoasi@gmail.com>
2021-01-19 09:10:32 +0000

    fix import path

--- packages/core/src/Footer/PrimaryFooter.js
+++ packages/core/src/Footer/PrimaryFooter.js
@@ -7,7 +7,7 @@ import { Grid, Hidden } from "@material-ui/core";
 
 import About from "./About";
 import Initiative from "./Initiative";
-import QuickLinks from "./QuickLinks";
+import QuickLinks from "../QuickLinks";
 import Logo from "../Logo";
 import Section from "../Section";
 
